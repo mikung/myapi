1d05e0b94223299186e9afa70d1cb62e5baf2b85
Teerawat Jordee <mikung555@Teerawats-MacBook-Pro.local>
2019-08-19 09:59:38 +0700

    new data

--- src/routes/caribou.ts
+++ src/routes/caribou.ts
@@ -14,7 +14,7 @@ const router: Router = Router();
 moment.locale('Thai');
 
 router.get('/', async (req: Request, res: Response) => {
-    console.log("data");
+    console.log("data2");
     try {
 
         console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
